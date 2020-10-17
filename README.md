6aca1347e5c9552a0b2edaa308ea8825c339339e
Harrison Beckerich <harrison@beckerich.org>
2020-10-16 20:10:21 -0700

    main created framework

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# bitcoinfiles-js
+# BitcoinFiles JS
+
+Upload large files directly to Bitcoin. 
+
+## Installation
+```bash
+```bash
+npm install @matterpool/bitcoinfiles-js
+``````
+
+## Usage
+
+Uploading a File
+```javascript
+```javascript
+const bitcoinfiles = require('@matterpool/bitcoinfiles-js');
+
+const workspaceId = '<insert-workspace-id>';
+const file = new File(['foo', 'bar'], 'foobar.txt');
+const formData = new FormData();
+formData.append('file', file);
+const upload = bitcoinfiles.upload(formData, workspaceId);
+``````
---
+++ index.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+
+const upload = async (file, sessionKey, dir) => {
+	let url = `/upload?tag=${sessionKey}`;
+
+	if (dir) {
+		url = `/upload?tag=${sessionKey}&dir=${dir}`;
+	}
+
+	const { data } = await axios.post(`https://api.bitcoinfiles.org${url}`, file, {
+		headers: {
+			'Content-Type': 'multipart/form-data',
+		},
+	});
+
+	return data;
+};
+
+const pay = async (rawtx) => {
+	const { data } = await axios.post(`https://api.bitcoinfiles.org/pay`, { rawtx });
+	return data;
+};
+
+const download = async (txid) => {
+	const { data } = await axios.get(`https://media.bitcoinfiles.org/${txid}`);
+	return data;
+};
+
+module.exports = { upload, pay, download };
